cbb638642111e7989d560a359c5a7d62b1739d47
nagode (Prof. dr. ) <marko.nagode@fs.uni-lj.si>
2025-11-06 08:38:20 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rebmix@305 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rebmix/demo/rebmix.weibullnormal.R
+++ pkg/rebmix/demo/rebmix.weibullnormal.R
@@ -17,91 +17,91 @@ data(weibullnormal, package = "rebmix")
 
 ########## FlexMix ##########
 
-library(flexmix)
+#library(flexmix)
 
 set.seed(8)
 
 # Weibull-normal mixture model.
 
-WNmodel <- function (formula = .~.) {
-  retval <- new("FLXMC", weighted = TRUE,
-    formula = formula, dist = "Weibull-normal",
-    name = "Weibull-normal parameters")
-
-  retval@defineComponent <- expression({
-    logLik <- function(x, y) {
-      logLik <- array()
-
-      for (i in 1:nrow(y)) {
-        logLik[i] <- dweibull(y[i, 1], shape = beta, scale = theta, log = TRUE) +
-                     dnorm(y[i, 2], mean = mean, sd = sd, log = TRUE)
-      }
-
-      logLik
-    }
-
-    new("FLXcomponent",
-      parameters = list(mean = mean, sd = sd, beta = beta, theta = theta), df = df, logLik = logLik)
-  })
-
-  retval@fit <- function(x, y, w, ...) {
-    n <- nrow(y)
-
-    mean <- sum(w * y[, 2]) / sum(w)
-
-    sd <- sqrt(sum(w * (y[, 2] - mean)^2) / sum(w))
-
-    beta <- 0.9
-
-    i <- 1; Error <- 1
-    while((i <= 1000) && (Error == 1)) {
-      T0 <- log(y[, 1])
-      T1 <- exp(T0 * beta)
-
-      A0 <- sum(w * T0)
-      A1 <- sum(w * T1 * T0)
-      A2 <- sum(w * T1)
-      A3 <- sum(w * T1 * T0 * T0)
-      A4 <- sum(w)
-
-      f <- (1.0 / beta) + A0 / A4 - A1 / A2
-      df <- ((A1 / A2) * (A1 / A2)) - (A3 / A2) - (1.0 / beta / beta)
-
-      dbeta <- f / df
-
-      beta <- beta - dbeta
-
-      if (is.nan(dbeta) || is.infinite(dbeta) || (beta <= 1.E-6)) {
-        stop("Not converged!")
-      }
-
-      if (abs(dbeta / beta) < 1.E-6) {
-        Error <- 0
-      }
-
-      i <- i + 1
-    }
-
-    theta <- exp(log(A2 / A4) * (1.0 / beta))
-
-    df <- 2 * ncol(y)
-
-    para <- list(sd = sd, mean = mean, beta = beta, theta = theta, n.obs = n)
-
-    with(para, eval(retval@defineComponent))
-  }
-
-  retval
-} ## WNmodel
+#WNmodel <- function (formula = .~.) {
+#  retval <- new("FLXMC", weighted = TRUE,
+#    formula = formula, dist = "Weibull-normal",
+#    name = "Weibull-normal parameters")
+#
+#  retval@defineComponent <- expression({
+#    logLik <- function(x, y) {
+#      logLik <- array()
+#
+#      for (i in 1:nrow(y)) {
+#        logLik[i] <- dweibull(y[i, 1], shape = beta, scale = theta, log = TRUE) +
+#                     dnorm(y[i, 2], mean = mean, sd = sd, log = TRUE)
+#      }
+#
+#      logLik
+#    }
+#
+#    new("FLXcomponent",
+#      parameters = list(mean = mean, sd = sd, beta = beta, theta = theta), df = df, logLik = logLik)
+#  })
+#
+#  retval@fit <- function(x, y, w, ...) {
+#    n <- nrow(y)
+#
+#    mean <- sum(w * y[, 2]) / sum(w)
+#
+#    sd <- sqrt(sum(w * (y[, 2] - mean)^2) / sum(w))
+#
+#    beta <- 0.9
+#
+#    i <- 1; Error <- 1
+#    while((i <= 1000) && (Error == 1)) {
+#      T0 <- log(y[, 1])
+#      T1 <- exp(T0 * beta)
+#
+#      A0 <- sum(w * T0)
+#      A1 <- sum(w * T1 * T0)
+#      A2 <- sum(w * T1)
+#      A3 <- sum(w * T1 * T0 * T0)
+#      A4 <- sum(w)
+#
+#      f <- (1.0 / beta) + A0 / A4 - A1 / A2
+#      df <- ((A1 / A2) * (A1 / A2)) - (A3 / A2) - (1.0 / beta / beta)
+#
+#      dbeta <- f / df
+#
+#      beta <- beta - dbeta
+#
+#      if (is.nan(dbeta) || is.infinite(dbeta) || (beta <= 1.E-6)) {
+#        stop("Not converged!")
+#      }
+#
+#      if (abs(dbeta / beta) < 1.E-6) {
+#        Error <- 0
+#      }
+#
+#      i <- i + 1
+#    }
+#
+#    theta <- exp(log(A2 / A4) * (1.0 / beta))
+#
+#    df <- 2 * ncol(y)
+#
+#    para <- list(sd = sd, mean = mean, beta = beta, theta = theta, n.obs = n)
+#
+#    with(para, eval(retval@defineComponent))
+#  }
+#
+#  retval
+#} ## WNmodel
 
 # Estimate number of components, component weights and component parameters by FlexMix.
 
-timeFlexMix <- system.time(weibullnormalestFlexMix <- flexmix(as.matrix(weibullnormal) ~ 1, k = 3, model = WNmodel()))
+#timeFlexMix <- system.time(weibullnormalestFlexMix <- flexmix(as.matrix(weibullnormal) ~ 1, k = 3, model = WNmodel()))
 
-parameters(weibullnormalestFlexMix)
-prior(weibullnormalestFlexMix)
-timeFlexMix
-summary(weibullnormalestFlexMix)
+#parameters(weibullnormalestFlexMix)
+#prior(weibullnormalestFlexMix)
+#timeFlexMix
+#summary(weibullnormalestFlexMix)
 
 ########## REBMIX ##########
 
